f102d4fb5c12382d06d540322db8516408087059
swastinuser.email <swastin95.sahoo@gmail.com>
2021-07-22 18:20:10 +0530

    commited by swastin

---
+++ csscards/script.js
@@ -0,0 +1,91 @@
+// "/* * {
+//     box-sizing: border-box;
+//     padding: 0;
+//     margin: 0;
+// }
+
+// body {
+//     /* background-image: linear-gradient(to right, #ff512f, #dd2476); */
+//     width: 100vw;
+//     height: 100vh;
+//     /* display: flex;
+//     justify-content: center;
+//     align-items: center; */
+//     background-color: rgb(148, 148, 148);
+//     position: relative;
+   
+// }
+
+// .cards {
+//      margin: 0 auto;
+//     display: flex;
+//     justify-content:center;
+//     align-items: center;
+//     width: 100%;
+//     height: 50%;
+//     border: 5px solid rgb(62, 150, 3);
+//     position: absolute;
+// 	top:0;
+// 	bottom: 0;
+// 	left: 0;
+// 	right: 0;
+  	
+// 	margin: auto;
+// }
+
+// .cards .card {
+//     background-color: #f2f2f2;
+//     width: 100%;
+//     height: 100%;
+//     margin: 0px 5px;
+//     border-radius: 27px 27px 0px 0px;
+//     border: 2PX solid blue;
+//     box-shadow: 4px 3px 8px -1px rgba(0, 0, 0, 0.75);
+
+// }
+
+// .card .card-header {
+//     position: relative;
+//     /* background-image: linear-gradient(to right, #3ca55c, #b5ac49);; */
+//     width: 100%;
+//     height: 100%;
+//     /* clip-path: circle(150px at center 0);*/
+
+// }
+
+// .card .card-header .banner {
+//     width: 100%;
+//     height: 40%;
+// }
+
+
+
+// .card .card-header .banner img {
+//     width: 100%;
+//     height: 100%;
+//     background-size: cover;
+//     object-fit: cover;
+//     border-radius: 27px 27px 0px 0px;
+// }
+
+// .card .card-header .profile-pic {
+//     width: 100px;
+//     height: 100px;
+//     border-radius:50%;
+//     position:absolute;
+//     left: 75px;
+//      bottom: 150px;
+//      background-color: white;
+//      box-shadow: 4px 3px 8px -1px rgba(0, 0, 0, 0.75);
+// }
+
+// .card .card-header .profile-pic img {
+//     width: 100px;
+//     height: 100px;
+//     border-radius:50%;
+
+// }
+// .card .card-body{
+// border: 2px solid red;
+
+// } */"
